54a57aad10d3fbde549cc926ee6efa6acb5caab5
huhen <9335143+huhen@users.noreply.github.com>
2026-09-17 16:35:11 +0300

    feat: общий футер (версия, GitHub, наверх) и кнопка редактирования на Архиве

--- web/embed.go
+++ web/embed.go
@@ -3,5 +3,5 @@ package web
 
 import "embed"
 
-//go:embed index.html admin.html app.js admin.js common.js parse.js style.css
+//go:embed index.html admin.html app.js admin.js common.js footer.js parse.js style.css
 var Files embed.FS
